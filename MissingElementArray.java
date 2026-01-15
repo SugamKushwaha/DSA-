121b20236af67087b702d4214a9cf4f3d07508da
Sugam <kushwahaSugam66@gmail.com>
2026-01-15 15:42:26 +0530

    new one

--- MissingElementArray.java
+++ MissingElementArray.java
@@ -3,15 +3,16 @@ public class MissingElementArray {
     public static void main(String[] args) {
         int[] arr = {1,3,5,2};
 
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr.length; j++) {
-                
-                if((i+1)!=j){
-                   System.out.print(arr[i]);
-                   return;
-                }
-            }
+        int a=(arr.length+1) * ((arr.length+1) + 1)/2;
+         int sum =0;
+        for(int i=0;i<arr.length;i++){
+             sum = sum+arr[i];
         }
+        
+        int result = a-sum;
+
+        System.out.println(result);
+        
 
     }
 }
