11c664eaed9f53f023e7c512e4bc2ebcc62d9475
Sugam <kushwahaSugam66@gmail.com>
2026-01-18 19:55:35 +0530

    new one

---
+++ SumOFTwo.java
@@ -0,0 +1,29 @@
+public class SumOFTwo {
+
+    public static int sum(){
+         int arr[] = {2,4,3,1,5,};
+        int target = 9;
+         int i=0; int j=arr.length-1;
+        while(i<j){
+            int sum= arr[i]+arr[j];
+
+            if(sum ==  target){
+            return i;
+            }
+            if(sum<target){
+                i++;
+            }
+              else if(sum>target){
+                j--;
+              }
+
+
+        }
+        return 0;
+    }
+    
+    public static void main(String[] args) {
+        
+       SumOFTwo.sum();
+    }
+}
