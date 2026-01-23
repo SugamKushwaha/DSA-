16530d960c4b323c319d095a843f3507d1aa3ecf
Sugam <kushwahaSugam66@gmail.com>
2026-01-23 22:31:35 +0530

    new one

--- Add1inArray.java
+++ Add1inArray.java
@@ -1,24 +1,28 @@
 
+import java.util.Arrays;
+
+
 public class Add1inArray {
  
     public static void main(String[] args) {
-        int[] arr = {1,2,3,8,9,0};
-        int[] arr1 = new int[arr.length];
-        int j=0;
-        for (int i = 0; i < arr.length; i++) {
-             j = j*10 + arr[i];
+     int[] arr = {9,8,9};
+       
+        int n = arr.length;
+
+        // Start from last digit
+        for (int i = n - 1; i >= 0; i--) {
+            if (arr[i] < 9) {
+                arr[i]++;
+                System.out.println(Arrays.toString(arr));
+                return ;
+            }
+            arr[i] = 0;
         }
-        int k=j+1; //124
 
-        System.out.println(k);
-
-        for (int i = 0; i < arr.length; i++) {
-                 int l= k % 10; //4,2,1
-                 k = k/10;
-                 arr1[i] = l;
-        }
-    
-        System.out.println(Arrays.toString(arr1));
-
+        // If all digits were 9 (e.g. [9,9,9])
+        int[] result = new int[n + 1];
+        result[0] = 1;
+        System.out.println(Arrays.toString(result));
     }
-}
+   }
+
