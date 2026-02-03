afa020d07e188208df5bec0cc10ee4e3f2f9a6a7
Sugam <kushwahaSugam66@gmail.com>
2026-02-03 20:38:17 +0530

    new one

--- ReverseString.java
+++ ReverseString.java
@@ -1,22 +1,16 @@
 import java.util.Arrays;
 
 public class ReverseString {
-
     public static void main(String[] args) {
-        
+        char[] c={'h','e','l','l','o'};
 
-        char[] c = {'h','e','l','l','o'};
-             int  b=c.length-1;
+        int b=c.length-1;
 
-        for(int i=0; i<b; i++){
-            char a=c[b];
-            c[b] =c[i];
+        for(int i=0;i<b;i++){
+            char a = c[b];
+            c[b]=c[i];
             c[i]=a;
-            b--;
         }
-
         System.out.println(Arrays.toString(c));
-        
     }
-    
 }
