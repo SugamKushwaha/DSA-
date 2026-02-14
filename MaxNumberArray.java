bcf3dc60fbd0cc1ef557ffb4b0ed7c672b7421bc
Sugam <kushwahaSugam66@gmail.com>
2026-02-14 20:56:15 +0530

    new one

---
+++ MaxNumberArray.java
@@ -0,0 +1,15 @@
+public class MaxNumberArray {
+    public static void main(String[] args) {
+        
+        int nums1[]={3,4,6,5};
+        int nums2[]={9,1,2,5,8,3};
+        
+        int k=5;
+
+        int arr[] = new int[k];
+
+        for(int i=0;i<arr.length;i++){
+
+        }
+    }
+}
