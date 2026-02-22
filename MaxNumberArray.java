2581d76e6cbb57c2aac7a12abaca71c441d93f09
Sugam <kushwahaSugam66@gmail.com>
2026-02-22 12:30:22 +0530

    new one

--- MaxNumberArray.java
+++ MaxNumberArray.java
@@ -1,15 +1,22 @@
+
+import java.util.HashMap;
+
 public class MaxNumberArray {
     public static void main(String[] args) {
-        
-        int nums1[]={3,4,6,5};
-        int nums2[]={9,1,2,5,8,3};
-        
-        int k=5;
+        int[] a={1,2,3};
+       int sum=0;
+       int k=3;
 
-        int arr[] = new int[k];
-
-        for(int i=0;i<arr.length;i++){
-
-        }
+       int res=0;
+       HashMap<Integer,Integer> f = new HashMap<>();
+               f.put(0, 1); 
+          for (int i = 0; i < a.length; i++) {
+              sum += a[i];
+              int q = sum - k;
+              int fre = f.getOrDefault(q, 0);  
+              res += fre;
+              f.put(sum, f.getOrDefault(sum, 0) + 1);  
+          }
+           System.out.println(res);
     }
 }
